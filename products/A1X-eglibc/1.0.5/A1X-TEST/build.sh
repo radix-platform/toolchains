6d9510616b32c4cfbc8299b7925c108362d44d6e
Andrey V.Kosteltsev <kx@radix.pro>
2014-01-14 01:14:40 +0000

    Added gcc-4.7.3; new toolchain version 1.0.5
    git-svn-id: https://svn.radix.pro/svn/toolchains/trunk@44 0df74aab-45ad-4799-93a1-122938110d35

---
+++ products/A1X-eglibc/1.0.5/A1X-TEST/build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+. ./.config
+
+export PATH=$TOOLCHAIN_PATH/bin:$PATH
+
+ARCH_FLAGS=" -march=armv7-a -mtune=cortex-a8"
+
+# without fp
+FPU_FLAGS=" "
+# NEON:
+#FPU_FLAGS=" -mfloat-abi=softfp -mfpu=neon -ftree-vectorize -fomit-frame-pointer -ffast-math"
+# VFP:
+#FPU_FLAGS=" -mfloat-abi=softfp -mfpu=vfp"
+
+$TARGET-gcc -g -gdwarf-2 -fomit-frame-pointer $ARCH_FLAGS $FPU_FLAGS -I$TOOLCHAIN_PATH/$TARGET/include -c -o main.o main.c
+$TARGET-gcc $ARCH_FLAGS $FPU_FLAGS -o main main.o
+
+$TARGET-objdump -x main > main.map
+
+$TARGET-strip main -o main.elf
+
+$TARGET-objcopy -O srec main.elf main.srec
+$TARGET-objcopy -O ihex main.elf main.hex
+$TARGET-objdump -S -d main.o > main.asm
+$TARGET-objdump -S -d main > full.program.asm
