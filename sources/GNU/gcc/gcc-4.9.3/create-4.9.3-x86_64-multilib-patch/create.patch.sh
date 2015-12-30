9d21a16c15c90d44c4cf0d4cd5d964cab4899618
Andrey V.Kosteltsev <kx@radix.pro>
2015-12-30 13:59:58 +0000

    sources for new versions of GNU GCC, gmp, mpfr
    git-svn-id: https://svn.radix.pro/svn/toolchains/trunk@124 0df74aab-45ad-4799-93a1-122938110d35

---
+++ sources/GNU/gcc/gcc-4.9.3/create-4.9.3-x86_64-multilib-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=4.9.3
+
+tar --files-from=file.list -xjvf ../gcc-$VERSION.tar.bz2
+mv gcc-$VERSION gcc-$VERSION-orig
+
+cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
+
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-x86_64-multilib.patch
+
+mv gcc-$VERSION-x86_64-multilib.patch ../../patches
+
+rm -rf ./gcc-$VERSION
+rm -rf ./gcc-$VERSION-orig
