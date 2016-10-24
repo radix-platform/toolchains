4e308771eb437c2d8dd0177ce7fe9bc8d112f584
Andrey V.Kosteltsev <kx@radix.pro>
2016-10-24 18:40:18 +0000

    preliminary commit of toolchain 1.0.10
    git-svn-id: https://svn.radix.pro/svn/toolchains/branches/toolchains-1.0.x@149 0df74aab-45ad-4799-93a1-122938110d35

--- products/A1X-boot/1.0.5/A1X-TEST/clean.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-rm -f full.program.asm
-rm -f main
-rm -f main.asm
-rm -f main.elf
-rm -f main.hex
-rm -f main.o
-rm -f main.srec
--- tools/dm644x/flash-tools/scripts/_flash-clean.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-TOOLCHAIN_PATH=/opt/toolchains/arm-DM644X-eabi-newlib
-TTY=/dev/ttyUSB0
-
-$TOOLCHAIN_PATH/bin/NOR-flasher --tty $TTY --erase
-
