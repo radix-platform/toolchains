cea90019bfc7386ba8eb1b6903d1c0a94ec55dd2
Andrey V.Kosteltsev <kx@radix.pro>
2016-10-24 07:55:55 +0000

    GCC-4.9.4 sources & patches
    git-svn-id: https://svn.radix.pro/svn/toolchains/trunk@146 0df74aab-45ad-4799-93a1-122938110d35

---
+++ sources/GNU/gcc/gcc-4.9.4/create-4.9.4-a2x-newlib-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=4.9.4
+
+tar --files-from=file.list -xjvf ../gcc-$VERSION.tar.bz2
+mv gcc-$VERSION gcc-$VERSION-orig
+
+cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
+
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-a2x-newlib.patch
+
+mv gcc-$VERSION-a2x-newlib.patch ../../patches
+
+rm -rf ./gcc-$VERSION
+rm -rf ./gcc-$VERSION-orig
