2e17b5318e262dedb7ec5df1db9229602cb1aaec
Andrey V.Kosteltsev <kx@radix.pro>
2016-10-26 21:35:23 +0000

    Amlogic kernel headers install patch
    git-svn-id: https://svn.radix.pro/svn/toolchains/branches/toolchains-1.1.x@158 0df74aab-45ad-4799-93a1-122938110d35

---
+++ sources/Linux/Amlogic/M201/create-3.10.33-20150117-headers-install-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=3.10.33-20150117
+
+tar --files-from=file.list -xJvf ../linux-aml-$VERSION.tar.xz
+mv linux-aml-$VERSION linux-aml-$VERSION-orig
+
+cp -rf ./linux-aml-$VERSION-new ./linux-aml-$VERSION
+
+diff -b --unified -Nr  linux-aml-$VERSION-orig  linux-aml-$VERSION > linux-aml-$VERSION-headers-install.patch
+
+mv linux-aml-$VERSION-headers-install.patch ../patches
+
+rm -rf ./linux-aml-$VERSION
+rm -rf ./linux-aml-$VERSION-orig
