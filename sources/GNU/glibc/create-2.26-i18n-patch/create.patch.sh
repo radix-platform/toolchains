435d7cac478c17797d8b240aff5853d76345ffaf
Andrey V.Kosteltsev <kx@radix.pro>
2017-08-14 07:22:59 +0000

    GNU Libc 2.26
    git-svn-id: https://svn.radix.pro/svn/toolchains/branches/toolchains-1.3.x@190 0df74aab-45ad-4799-93a1-122938110d35

---
+++ sources/GNU/glibc/create-2.26-i18n-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.26
+
+tar --files-from=file.list -xJvf ../glibc-$VERSION.tar.xz
+mv glibc-$VERSION glibc-$VERSION-orig
+
+cp -rf ./glibc-$VERSION-new ./glibc-$VERSION
+
+diff -b --unified -Nr  glibc-$VERSION-orig  glibc-$VERSION > glibc-$VERSION-i18n.patch
+
+mv glibc-$VERSION-i18n.patch ../patches
+
+rm -rf ./glibc-$VERSION
+rm -rf ./glibc-$VERSION-orig
