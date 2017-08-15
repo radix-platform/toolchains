670b1a156e9873301ecf05cf2a8dacf836413455
Andrey V.Kosteltsev <kx@radix.pro>
2017-08-15 21:06:02 +0000

    GCC7: renamed unwind patch, added libsanitizer changes related to glibc-2.26
    git-svn-id: https://svn.radix.pro/svn/toolchains/branches/toolchains-1.3.x@193 0df74aab-45ad-4799-93a1-122938110d35

--- sources/GNU/gcc/gcc-7.1.0/create-7.1.0-glibc-2.26-patch/create.patch.sh
+++ sources/GNU/gcc/gcc-7.1.0/create-7.1.0-glibc-2.26-patch/create.patch.sh
@@ -7,9 +7,9 @@ mv gcc-$VERSION gcc-$VERSION-orig
 
 cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
 
-diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-unwind.patch
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-glibc-2.26.patch
 
-mv gcc-$VERSION-unwind.patch ../../patches
+mv gcc-$VERSION-glibc-2.26.patch ../../patches
 
 rm -rf ./gcc-$VERSION
 rm -rf ./gcc-$VERSION-orig
