5b1d3e6fd323fd63bc1587537c8c14093d54879f
Andrey V.Kosteltsev <kx@radix.pro>
2017-08-15 07:29:08 +0000

    GCC 7.1.0 linux-unwind patch
    git-svn-id: https://svn.radix.pro/svn/toolchains/branches/toolchains-1.3.x@192 0df74aab-45ad-4799-93a1-122938110d35

---
+++ sources/GNU/gcc/gcc-7.1.0/create-7.1.0-unwind-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=7.1.0
+
+tar --files-from=file.list -xjvf ../gcc-$VERSION.tar.bz2
+mv gcc-$VERSION gcc-$VERSION-orig
+
+cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
+
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-unwind.patch
+
+mv gcc-$VERSION-unwind.patch ../../patches
+
+rm -rf ./gcc-$VERSION
+rm -rf ./gcc-$VERSION-orig
