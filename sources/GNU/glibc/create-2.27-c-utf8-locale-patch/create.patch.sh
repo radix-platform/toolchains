5c9f7006bd7b2eecc83c8d03aefd6501d2159b65
Andrey V.Kosteltsev <kx@radix.pro>
2018-03-03 19:29:21 +0000

    move to glibc-2.27
    git-svn-id: https://svn.radix.pro/svn/toolchains/branches/toolchains-1.2.x@200 0df74aab-45ad-4799-93a1-122938110d35

---
+++ sources/GNU/glibc/create-2.27-c-utf8-locale-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=2.27
+
+tar --files-from=file.list -xJvf ../glibc-$VERSION.tar.xz
+mv glibc-$VERSION glibc-$VERSION-orig
+
+cp -rf ./glibc-$VERSION-new ./glibc-$VERSION
+
+diff -b --unified -Nr  glibc-$VERSION-orig  glibc-$VERSION > glibc-$VERSION-c-utf8-locale.patch
+
+mv glibc-$VERSION-c-utf8-locale.patch ../patches
+
+rm -rf ./glibc-$VERSION
+rm -rf ./glibc-$VERSION-orig
---
+++ sources/Linux/v4.x/create-4.14.23-sparc64-CAS-emulation-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+VERSION=4.14.23
+
+tar --files-from=file.list -xJvf ../linux-$VERSION.tar.xz
+mv linux-$VERSION linux-$VERSION-orig
+
+cp -rf ./linux-$VERSION-new ./linux-$VERSION
+
+diff -b --unified -Nr  linux-$VERSION-orig  linux-$VERSION > linux-$VERSION-sparc64-CAS-emulation.patch
+
+mv linux-$VERSION-sparc64-CAS-emulation.patch ../patches
+
+rm -rf ./linux-$VERSION
+rm -rf ./linux-$VERSION-orig
