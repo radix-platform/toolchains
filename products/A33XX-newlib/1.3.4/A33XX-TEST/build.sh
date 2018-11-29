4331e03394692ca2eb68fe268414080397c96cff
kx <kx@radix.pro>
2018-11-29 13:29:34 +0300

    ARMv6-M (Cortex-m0) Toolchain for RK3399 power management firmware

---
+++ products/A33XX-newlib/1.3.4/A33XX-TEST/build.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+. ./.config
+
+export PATH=$TOOLCHAIN_PATH/bin:$PATH
+
+DEBUG_FLAGS="-gdwarf-2"
+ARCH_FLAGS="-mthumb -march=armv6-m -mcpu=cortex-m0"
+#NO_STD_FLAGS=" -nostdlib -nostartfiles"
+
+$TARGET-gcc -g $DEBUG_FLAGS $ARCH_FLAGS $FPU_FLAGS -I$TOOLCHAIN_PATH/$TARGET/include -c -o main.o main.c
+
+$TARGET-gcc ${NO_STD_FLAGS} $ARCH_FLAGS $FPU_FLAGS $LDFLAGS -o main main.o
+
+$TARGET-objdump -x main > main.map
+
+$TARGET-strip main -o main.elf
+
+$TARGET-objcopy -O srec main.elf main.srec
+$TARGET-objcopy -O ihex main.elf main.hex
+$TARGET-objdump -S -d main.o > main.asm
+$TARGET-objdump -S -d main > full.program.asm
