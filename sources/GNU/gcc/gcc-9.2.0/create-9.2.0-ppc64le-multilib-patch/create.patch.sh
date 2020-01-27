76b9c358776a493d4e940f32de3bb7909b2c7845
kx <kx@radix.pro>
2020-01-27 14:26:54 +0300

    gcc-9.2.0

---
+++ sources/GNU/gcc/gcc-9.2.0/create-9.2.0-ppc64le-multilib-patch/create.patch.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+VERSION=9.2.0
+
+tar --files-from=file.list -xJvf ../gcc-$VERSION.tar.xz
+mv gcc-$VERSION gcc-$VERSION-orig
+
+cp -rf ./gcc-$VERSION-new ./gcc-$VERSION
+
+diff -b --unified -Nr  gcc-$VERSION-orig  gcc-$VERSION > gcc-$VERSION-ppc64le-multilib.patch
+
+mv gcc-$VERSION-ppc64le-multilib.patch ../../patches
+
+rm -rf ./gcc-$VERSION
+rm -rf ./gcc-$VERSION-orig
